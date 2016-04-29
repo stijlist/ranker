396cb5292ea9e0a9dd631daf8e98b3ee8f6a787e
Bert Muthalaly <bert@somethingdoneright.net>
2016-04-29 00:57:39 -0400

    Cleaned up index.html, and start work

--- src/ranker/core.cljs
+++ src/ranker/core.cljs
@@ -1,9 +1,17 @@
-(ns ranker.core)
+(ns ranker.core
+  (:require [clojure.string :as string]))
+
 (enable-console-print!)
 (.timeEnd js/console "cljs load")
 
+(def state (atom []))
+
+(defn append-msg-to-state [event msg]
+  (swap! state concat (string/split msg "\n"))
+  (println @state))
+
 (.. (js/require "electron") 
   -ipcRenderer 
-  (on "stdin" (fn [e msg] (println msg))))
+  (on "stdin" append-msg-to-state))
 
 (.timeEnd js/console "start")
